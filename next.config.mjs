66080d4ec34197a06a9b586fb5fd4a36dab015b2
yiyan2608-deli <yiyan2608@gmail.com>
2026-07-22 20:16:37 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 必须加这一行，告诉 Next.js 导出静态 HTML
+  output: 'export', // 💡 必须加这一行，告诉 Next.js 导出静态 HTML 网页
   images: {
-    unoptimized: true, // 必须加这一行，禁用图片优化，否则打包报错
+    unoptimized: true, // 💡 必须加这一行，禁用静态模式下不支持的图片优化
   },
 };
 
