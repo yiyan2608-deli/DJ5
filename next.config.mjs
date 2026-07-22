a296a893f824d92e13f351b8dbe21e1237592a3e
yiyan2608-deli <yiyan2608@gmail.com>
2026-07-22 20:15:22 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export', // 必须加这一行，告诉 Next.js 导出静态 HTML
   images: {
-    unoptimized: true,
+    unoptimized: true, // 必须加这一行，禁用图片优化，否则打包报错
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
